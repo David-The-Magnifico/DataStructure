1b70ff1996a5c76c985bc6a658e7b6ed75169f19
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-03-03 21:47:39 +0100

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,1 +1,12 @@
 import java,util.*;
+
+public class Queue{
+  public static void main(String[] args){
+    Queue<String> str_queue = new LinkedList<>();
+    str_queue.add("one");
+    str_queue.add("two");
+    str_queue.add("three");
+    str_queue.add("four");
+    Syystem.out.println("The Queue contains:" + str_queue);
+  }
+}
